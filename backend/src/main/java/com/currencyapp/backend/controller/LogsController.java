5469f8dd4d07c06c2b5d15786b9b5adb4907c9c5
VaclavMoucha <vaclav8moucha@gmail.com>
2026-04-03 10:33:40 +0200

    Exceptions + logsContorller

---
+++ backend/src/main/java/com/currencyapp/backend/controller/LogsController.java
@@ -0,0 +1,22 @@
+package com.currencyapp.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.currencyapp.backend.model.LogEntry;
+import com.currencyapp.backend.repository.LogRepository;
+
+@RestController
+@RequestMapping("/api/logs")
+public class LogsController {
+    @Autowired
+    private LogRepository logRepository;
+    @GetMapping
+    public List<LogEntry> getLogs() {
+        return logRepository.getLogs();
+    }
+}
